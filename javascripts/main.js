1e86b4a0bd95efe6fa98906bc877a50ffc683730
sonyqburst <sonya@qburst.com>
2015-05-29 12:52:20 +0530

    gogle chart added for testing purpose

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,28 @@
 console.log('This would be the main JS file.');
+
+var userRawData;
+
+function getUserData()
+{
+
+}
+
+function createCHartDataByUser(user)
+{
+
+
+	return [];
+}
+
+
+
+function renderChart(createCHartDataByUser (userRawData) )
+{
+
+}
+
+
+function onFilterSelection(filterType)
+{
+	
+}
